0bfe638ce6ddd2585734f6a1c32ccd70d7807089
trantu2641 <trantu2641@gmail.com>
2026-09-12 13:50:45 +0700

    Update KRRootListController.m

--- prefs/KeyboardRoundedPrefs/KRRootListController.m
+++ prefs/KeyboardRoundedPrefs/KRRootListController.m
@@ -1,12 +1,42 @@
 #import "KRRootListController.h"
+#import <Preferences/PSControlTableCell.h>
+#import <spawn.h>
+
+@interface KRRespringCell : PSControlTableCell
+@end
+
+@implementation KRRespringCell
+
+- (void)controlTapped:(id)sender
+{
+    pid_t pid;
+    const char *args[] = {
+        "/usr/bin/killall",
+        "SpringBoard",
+        NULL
+    };
+
+    posix_spawn(
+        &pid,
+        args[0],
+        NULL,
+        NULL,
+        (char * const *)args,
+        NULL
+    );
+}
+
+@end
+
 
 @implementation KRRootListController
 
 - (NSArray *)specifiers
 {
     if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root"
-                                                  target:self];
+        _specifiers =
+            [self loadSpecifiersFromPlistName:@"Root"
+                                        target:self];
     }
 
     return _specifiers;
