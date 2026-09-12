3748485019ea6a0cb8514e024e471546ab1523e3
trantu2641 <trantu2641@gmail.com>
2026-09-12 09:40:29 +0700

    Create KRRootListController.m

---
+++ prefs/KeyboardRoundedPrefs/KRRootListController.m
@@ -0,0 +1,15 @@
+#import "KRRootListController.h"
+
+@implementation KRRootListController
+
+- (NSArray *)specifiers
+{
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root"
+                                                 target:self];
+    }
+
+    return _specifiers;
+}
+
+@end
